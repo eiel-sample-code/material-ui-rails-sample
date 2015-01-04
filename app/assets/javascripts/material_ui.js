1c0d29b5670ef83b6ba0371c0fad2794f1fe0184
Tomohiko Himura <eiel.hal@gmail.com>
2015-01-04 21:53:38 +0900

    initial commit

---
+++ app/assets/javascripts/material_ui.js
@@ -0,0 +1,10 @@
+var React = require('react'),
+  mui = require('material-ui'),
+  RaisedButton = mui.RaisedButton;
+
+$(function(){
+  React.render(
+    <RaisedButton label="hello" />,
+    document.body
+  );
+});
